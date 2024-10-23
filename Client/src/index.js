f04b7108d5f4199ba5eae980fee5b2370cd71ca3
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2024-10-23 22:59:25 +0530

    hoja ab to

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,5 @@
-import React from '.pnpm/react@18.3.1/node_modules/react';
-import { createRoot } from '.pnpm/react-dom@18.3.1_react@18.3.1/node_modules/react-dom/client';
+import React from 'react'; // Corrected import for React
+import { createRoot } from 'react-dom/client'; // Corrected import for createRoot
 
 import './styles.css';
 import App from './App.jsx';
